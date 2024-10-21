1b4e25f65291788ee9b0e194c4a11a8670923f85
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2024-10-21 03:04:44 +0300

    made

--- src/Slides/Slide1.jsx
+++ src/Slides/Slide1.jsx
@@ -4,11 +4,7 @@ function Slide1() {
   return (
     <div className="slide slide1">
       <h1>Павло Скоропатський</h1>
-      <img
-        className="titleImg"
-        src="../../public/img/PavloSkoropadsky.jpg"
-        alt="#"
-      />
+      <img className="titleImg" src="/img/PavloSkoropadsky.jpg" alt="#" />
       <p className="titleText">
         Гетьман Української держави з 29 квітня по 14 грудня 1918(6.5 місяців).
         Народився у травні 1873, Вісбаден, Німецька імперія помер 26 квітня
